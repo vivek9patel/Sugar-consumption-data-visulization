00bbadfefb024b3ec5d6f657d330f27870d254d5
Vivek Patel <vivek.p9737@gmail.com>
2023-04-17 22:45:56 -0700

    description + extra credit 🚀

--- js/index.js
+++ js/index.js
@@ -1,20 +1,10 @@
-const svgMargin = { top: 100, right: 100, bottom: 110, left: 100 };
 const highestSugarConsumptionCountries = [
   "Louisiana",
   "Kentucky",
   "West Virginia",
 ]; // "Mississippi"
-const lowestSugarConsumptionCountries = ["Vermont", "Connecticut", "New York"]; // "Minnesota"
-
-// const width =
-//     Math.min(700, window.innerWidth - 10) - svgMargin.left - svgMargin.right,
-//   height = Math.min(
-//     width,
-//     window.innerHeight - svgMargin.top - svgMargin.bottom - 20
-//   );
-
-const width = 600 - svgMargin.left - svgMargin.right;
-const height = width;
+const lowestSugarConsumptionCountries = ["Vermont", "Connecticut", "New York"];// "Minnesota"
+const regionToColor = ["#1577B7", "#EDC951", "rgb(107,61,155)"];
 
 const getCSVdata = () =>
   Promise.all([d3.csv("data/education_sugar_cdc_2003.csv")]);
@@ -24,7 +14,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     await getData();
 
   drawRadarChart("highest-consumption", highestConsumptionCountryData);
-  drawRadarChart("lowest-consumption", lowestConsumptionCountryData);
+  // drawRadarChart("lowest-consumption", lowestConsumptionCountryData);
+  drawScatterPlot("lowest-consumption", lowestConsumptionCountryData);
 });
 
 async function getData() {
@@ -51,15 +42,16 @@ async function getData() {
 }
 
 function drawRadarChart(id, data) {
+  const svgMargin = { top: 100, right: 100, bottom: 110, left: 100 };
+  const width = 600 - svgMargin.left - svgMargin.right;
+  const height = 600 - svgMargin.top - svgMargin.bottom;
   const radarChartConfig = {
-    w: width,
-    h: height,
     totalInnerCirclePartitions: 6,
     maxDataValue: 60,
   };
 
   const axisList = data[0].map((i, j) => i.axis),
-    radius = Math.min(radarChartConfig.w / 2, radarChartConfig.h / 2),
+    radius = Math.min(width / 2, height / 2),
     numberPr = d3.format(".2"),
     chartPiece = (Math.PI * 2) / axisList.length;
 
@@ -67,23 +59,21 @@ function drawRadarChart(id, data) {
     .scaleLinear()
     .range([0, radius])
     .domain([0, radarChartConfig.maxDataValue]);
-
-  const regionToColor = ["#1577B7","#EDC951","rgb(107,61,155)"]
-  const color = d3.scaleOrdinal().range(regionToColor)
+  const color = d3.scaleOrdinal().range(regionToColor);
 
   const svg = d3
     .select(`svg#${id}`)
-    .attr("width", radarChartConfig.w + svgMargin.left + svgMargin.right)
-    .attr("height", radarChartConfig.h + svgMargin.top + svgMargin.bottom);
+    .attr("width", width + svgMargin.left + svgMargin.right)
+    .attr("height", height + svgMargin.top + svgMargin.bottom);
 
   const g = svg
     .append("g")
     .attr(
       "transform",
       "translate(" +
-        (radarChartConfig.w / 2 + svgMargin.left) +
+        (width / 2 + svgMargin.left) +
         "," +
-        (radarChartConfig.h / 2 + svgMargin.top) +
+        (height / 2 + svgMargin.top) +
         ")"
     );
 
@@ -121,9 +111,11 @@ function drawRadarChart(id, data) {
     .style("font-size", "12px")
     .attr("fill", "#737373")
     .text(function (d, i) {
-      return numberPr(
-        (radarChartConfig.maxDataValue * d) /
-          radarChartConfig.totalInnerCirclePartitions
+      return (
+        numberPr(
+          (radarChartConfig.maxDataValue * d) /
+            radarChartConfig.totalInnerCirclePartitions
+        ) + " t/d"
       );
     });
 
@@ -205,7 +197,8 @@ function drawRadarChart(id, data) {
     })
     .style("fill-opacity", 0.4)
     .on("mouseover", function (d, i) {
-      d3.selectAll(".radarArea")
+      svg
+        .selectAll(".radarArea")
         .transition()
         .duration(200)
         .style("fill-opacity", 0.1);
@@ -213,7 +206,8 @@ function drawRadarChart(id, data) {
       d3.select(this).transition().duration(200).style("fill-opacity", 0.7);
     })
     .on("mouseout", function () {
-      d3.selectAll(".radarArea")
+      svg
+        .selectAll(".radarArea")
         .transition()
         .duration(200)
         .style("fill-opacity", 0.4);
@@ -293,11 +287,11 @@ function drawRadarChart(id, data) {
 
   const tooltip = g.append("text").attr("class", "tooltip").style("opacity", 0);
 
-  // label for regionToColor, put it in bottom left corner
-  const legend = g.append("g").attr("class", "colorLegend")
-    .attr("transform", "translate(" + -120 + "," + (height-100) + ")");
+  const legend = g
+    .append("g")
+    .attr("class", "colorLegend")
+    .attr("transform", "translate(" + -120 + "," + (height - 100) + ")");
 
-  
   legend
     .selectAll("rect")
     .data(regionToColor)
@@ -318,7 +312,7 @@ function drawRadarChart(id, data) {
     .data(regionToColor)
     .enter()
     .append("text")
-    .text(function (d,i) {
+    .text(function (d, i) {
       return data[i][0].state;
     })
     .attr("x", function (d, i) {
@@ -329,5 +323,133 @@ function drawRadarChart(id, data) {
     .style("fill", "black")
     .style("text-anchor", "start")
     .style("alignment-baseline", "middle");
+}
 
+function drawScatterPlot(id, data) {
+  const svgMargin = { top: 100, right: 100, bottom: 110, left: 100 };
+  const width = 600;
+  const height = 600 - svgMargin.top - svgMargin.bottom;
+  const newData = [];
+  const dimensions = [
+    "<High school",
+    "High school",
+    "Some college",
+    "College graduate",
+    "Employed",
+    "Not employed",
+    "Retired",
+  ];
+
+  data.forEach((d) => {
+    const obj = {};
+    d.forEach((e) => {
+      obj[e.axis] = e.value;
+    });
+    newData.push({
+      state: d[0].state,
+      ...obj,
+    });
+  });
+
+  data = newData;
+
+  const svg = d3
+    .select(`svg#${id}`)
+    .attr("width", width)
+    .attr("height", height + svgMargin.top + svgMargin.bottom)
+    .append("g")
+    .attr(
+      "transform",
+      "translate(" + svgMargin.left + "," + svgMargin.top + ")"
+    );
+
+  const color = d3
+    .scaleOrdinal()
+    .domain(lowestSugarConsumptionCountries)
+    .range(regionToColor);
+
+  const y = {};
+  dimensions.forEach((name) => {
+    y[name] = d3.scaleLinear().domain([0, 60]).range([height, 0]);
+  });
+
+  const x = d3.scalePoint().range([-50, width-120]).domain(dimensions);
+
+  const path = (d) => d3.line()(dimensions.map((p) => [x(p), y[p](d[p])]));
+
+  svg
+    .selectAll("myPath")
+    .data(data)
+    .join("path")
+    .attr("class", function (d) {
+      return "line";
+    })
+    .attr("d", path)
+    .style("fill", "none")
+    .style("stroke", function (d) {
+      return color(d.state);
+    });
+
+  svg
+    .selectAll("myAxis")
+    .data(dimensions)
+    .enter()
+    .append("g")
+    .attr("class", "axis")
+    .attr("transform", function (d) {
+      return `translate(${x(d)})`;
+    })
+    .each(function (d) {
+      d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d]));
+    })
+    .append("text")
+    .style("text-anchor", "middle")
+    .attr("x", -12)
+    .attr("y", -10)
+    .text(function (d) {
+      return d;
+    })
+    .style("fill", "black")
+    .style("font-size", "12px")
+    .style("font-weight", "bold");
+
+  const legend = svg
+    .append("g")
+    .attr("class", "colorLegend")
+    .attr(
+      "transform",
+      "translate(" + 60 + "," + (height + svgMargin.bottom - 12) + ")"
+    );
+
+  legend
+    .selectAll("rect")
+    .data(regionToColor)
+    .enter()
+    .append("rect")
+    .attr("x", function (d, i) {
+      return i * 100;
+    })
+    .attr("y", function (d, i) {
+      return 0;
+    })
+    .attr("width", 10)
+    .attr("height", 10)
+    .style("fill", (d) => d);
+
+  legend
+    .selectAll("text")
+    .data(regionToColor)
+    .enter()
+    .append("text")
+    .text(function (d, i) {
+      return lowestSugarConsumptionCountries[i];
+    })
+    .attr("x", function (d, i) {
+      return i * 100 + 15;
+    })
+    .attr("y", 6)
+    .style("font-size", "12px")
+    .style("fill", "black")
+    .style("text-anchor", "start")
+    .style("alignment-baseline", "middle");
 }
